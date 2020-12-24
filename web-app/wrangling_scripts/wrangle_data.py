cea70445a5b8c5c179360ae3f89faac7bc0c9f6d
Lars Tinnefeld <50445480+LarsTinnefeld@users.noreply.github.com>
2020-12-24 09:57:17 -0500

    Visializations
    Data preparations:
    - hourly Irradiation for May 25
    - total DC Power for both plants
    
    Added various chart types:
    chart 2: bar
    chart 3: scatter with dots
    chart 4: pie

--- web-app/wrangling_scripts/wrangle_data.py
+++ web-app/wrangling_scripts/wrangle_data.py
@@ -49,10 +49,10 @@ def return_figures():
     x_val = df_daily['date']
     y_val = df_daily['DC_POWER']
     graph_two.append(
-        go.Scatter(
+        go.Bar(
             x = x_val,
             y = y_val,
-            mode = 'lines',
+            #mode = 'lines',
             name = 'DC_POWER'
         )
     )
@@ -64,10 +64,62 @@ def return_figures():
             )
         )
 
+    graph_three = []
+    df = import_gen('data/Plant_1_Weather_Sensor_Data.csv')
+    df_may_25 = df.loc[df.date == pd.to_datetime('2020-05-25')][['IRRADIATION', 'DATE_TIME']]
+    x_val = df_may_25['DATE_TIME']
+    y_val = df_may_25['IRRADIATION']
+    graph_three.append(
+        go.Scatter(
+            x = x_val,
+            y = y_val,
+            mode = 'markers',
+            name = 'DC_POWER',
+            marker=dict(
+                color='LightSkyBlue',
+                size=5,
+                line=dict(
+                    color='MediumPurple',
+                    width=2
+                    )
+                )
+            )
+        )
+
+    layout_three = dict(
+        title = 'Irradiation for May 25, 2020',
+        xaxis = dict(
+            title = 'Timeline',
+            autoticks = False
+            )
+        )
+
+    graph_four = []
+    df_x = daily_gen_grouping('data/Plant_1_Generation_Data.csv')
+    df_y = daily_gen_grouping('data/Plant_2_Generation_Data.csv')
+    df_xy = df_x.merge(df_y, on='date')
+
+    labels = ['Plant 1', 'Plant 2']
+    value_x = df_xy['DC_POWER_x'].sum()
+    value_y = df_xy['DC_POWER_y'].sum()
+    values = [value_x, value_y]
+
+    graph_four.append(
+        go.Pie(
+            labels = labels,
+            values = values,
+            name = 'DC_POWER comparison',
+            )
+        )
+
+    layout_four = dict(
+        title = 'DC Power comparison'
+        )
+
     figures = []
     figures.append(dict(data = graph_one, layout = layout_one))
     figures.append(dict(data = graph_two, layout = layout_two))
-    figures.append(dict(data = graph_two, layout = layout_two))
-    figures.append(dict(data = graph_two, layout = layout_two))
+    figures.append(dict(data = graph_three, layout = layout_three))
+    figures.append(dict(data = graph_four, layout = layout_four))
 
     return figures
